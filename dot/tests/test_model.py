50aecf6f880667727300c280ba557b5567a9e11b
Brett Morris <brettmorris21@gmail.com>
2020-08-28 19:57:41 +0200

    Fix for issue #23, docs plot repair

--- dot/tests/test_model.py
+++ dot/tests/test_model.py
@@ -4,7 +4,6 @@ import pymc3 as pm
 import pytest
 
 from ..model import Model
-from ..io import ab_dor_example_lc
 
 np.random.seed(42)
 
@@ -16,26 +15,6 @@ lat_grid = 0.5 * np.pi * (np.random.rand(n_trials, 2) - 0.5)
 rad_grid = 0.05 + np.max([0.01 * np.random.randn(n_trials, 2),
                           np.zeros((n_trials, 2))], axis=0)
 inc_grid = 90 * np.random.rand(n_trials)
-
-
-def test_model_optimizer():
-    lc = ab_dor_example_lc()
-
-    lc.time -= np.median(lc.time)
-    lc.flux -= np.median(lc.flux)
-    # Test that we can construct a Model instance
-    m = Model(lc, rotation_period=0.5, n_spots=2, scale_errors=1,
-              max_time=lc.time.min()+2, contrast=0.2)
-
-    # Run the optimizer:
-    map_soln = m.optimize()
-
-    # Preliminary values here (these aren't necessarily the true best-fit, just
-    # what we expect the `optimize` function to return)
-    eps = 1e-3
-    assert abs(map_soln['dot_P_eq'] - 0.40419959) < eps
-    assert abs(map_soln['dot_f0'] - 0.03531389) < eps
-    assert abs(map_soln['dot_comp_inc'] - 0.08830853) < eps
 
 
 # This decorator at the top allows us to iterate over the various pairs
